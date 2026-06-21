2c6e444548e56794f8265a82767bdbea82c7cf31
wango044 <kindlyfluorine2@gmail.com>
2026-06-22 01:32:14 +0300

    Make site builds Vercel-ready

--- sites/private-investor-club/vite.config.js
+++ sites/private-investor-club/vite.config.js
@@ -1,1 +1,6 @@
-export { default } from '../../tooling/vite/react-app.js'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+})
